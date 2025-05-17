a4eaca82b4d5a2ea3b1195cfd63e88b0811ea9cb
unknown <kittu.wgl1@gmail.com>
2025-05-17 19:47:57 +0530

    static commit

--- build_files.sh
+++ build_files.sh
@@ -4,7 +4,7 @@ echo "BUILD START"
 python3 -m pip install -r requirements.txt
 
 # Create the output directory if it doesn't exist
-mkdir -p staticfiles
+mkdir -p staticfiles_build
 
 # Collect static files
 python3 manage.py collectstatic --noinput --clear
