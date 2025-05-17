0cae1a360120e948c4cab3fbbbdb4fa62413d3c9
unknown <kittu.wgl1@gmail.com>
2025-05-17 20:00:23 +0530

    static commit

--- pms/settings.py
+++ pms/settings.py
@@ -77,17 +77,17 @@ WSGI_APPLICATION = 'pms.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-database_url = os.environ.get("DATABASE_URL")
-DATABASES ={
-    'default' : dj_database_url.parse(database_url)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# database_url = os.environ.get("DATABASE_URL")
+# DATABASES ={
+#     'default' : dj_database_url.parse(database_url)
+# }
 
 
 # Password validation
@@ -124,7 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
